d10a8813d2574e8d9891e707bee0e72f54eaa308
Andrey Sidorov <sidorares@yandex.com>
2015-04-01 11:11:22 +1100

    debug log

--- build/publish.sh
+++ build/publish.sh
@@ -2,16 +2,17 @@
 
 cd "$(dirname "$0")"
 cd ../../gitgit-gh-pages
+ls -l
 git checkout -- .
 git clean -dfx
 git fetch
 git rebase
 rm -Rf *
 cd ../gitgit/build
-
+ls -l
 # generate code
 ./node_modules/.bin/marked ../index.md > ../../gitgit-gh-pages/index.html
-
+ls -l ../..
 # cd into repo
 cd ../../gitgit-gh-pages
 git add --all
--- build/setup.sh
+++ build/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-
+echo "SETUP!"
 cd ../..
 git clone "https://sidorares@github.com/sidorares/gitgit.git" gitgit-gh-pages
 cd gitgit-gh-pages
 git checkout origin/gh-pages
 git checkout -b gh-pages
+ls -l
 git push --set-upstream origin gh-pages
 cd ../gitgit/build
+ls -l
