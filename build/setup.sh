2cbd4b0f7a1c88f7f9bc5c70955dac86650d2b15
Andrey Sidorov <sidorares@yandex.com>
2015-04-01 11:50:21 +1100

    dump

--- build/setup.sh
+++ build/setup.sh
@@ -2,6 +2,9 @@
 
 echo "SETUP!"
 cd ../..
+git config --global user.email 'sidorares@yandex.com'
+git config --global user.name 'Andrey Sidorov'
+
 git clone "https://sidorares@github.com/sidorares/gitgit.git" gitgit-gh-pages
 cd gitgit-gh-pages
 git checkout origin/gh-pages
